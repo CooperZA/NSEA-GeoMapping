8fb8f324a7740f90b1786b798ef91b9c5c28453d
CooperZA <zach@raincitysolutions.com>
2020-08-06 13:52:29 -0700

    add session helpers

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,9 +6,9 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 // admin model
 let Admin = require('../models/admin.model');
-
-// import signUp validation
+// import signUp validation and helper functions
 import { signUp } from '../validations/admin';
+import { parseError, sessionizeUser } from '../util/helpers';
 
 // get login form
 router.route('/').get((req, res) => {
@@ -31,25 +31,15 @@ router.route('/').post((req, res) => {
         Password,
     });
 
+    const sessionUser = sessionizeUser(newAdmin);
+
     newAdmin.save()
-        .then(() => res.send({ userId: newUser.id, Username}))
-        .catch(err => res.status(400).json('Error(Admin add new router): ' + err));
+        .then(() => {
+            req.session.user = sessionUser;
+            res.send(sessionUser);
+        })
+        .catch(err => res.status(400).json('Error(Admin add new router): ' + parseError(err)));
 
-    
-
-    // Admin.findOne({ Username: Username })
-    //     .then(user => {
-    //         console.log("user from login", user);
-    //         if(!user){
-    //             // user not found
-    //             res.sendStatus(204);
-    //         }else{
-    //             // compare passwords
-    //             bcrypt.compare(Password, user.Password)
-    //                 .then(pwMatch => pwMatch ? res.sendStatus(200) : res.sendStatus(204))
-    //                 .catch(err => res.status(400).json('Error(post login form): ' + err));
-    //         }   
-    //     })
 });
 
 module.exports = router;
