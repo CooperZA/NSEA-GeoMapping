0f6b2b8f70a8345a8b3e8e44dbdc22e81f671bd0
CooperZA <zach@raincitysolutions.com>
2020-08-21 09:14:03 -0700

    functionize app rendering

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store/store'
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { checkLoggedIn } from "./util/session";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let preloadedState = {};
+
+const renderApp = preloadedState => {
+  const store = configureStore(preloadedState);
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+(async () => renderApp(await checkLoggedIn()))();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
