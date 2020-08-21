9e0a03c634af846ac255b9ac44cabfecda61caf4
CooperZA <zach@raincitysolutions.com>
2020-08-21 09:15:08 -0700

    add login/out handlers

---
+++ app/client/src/util/session.js
@@ -0,0 +1,23 @@
+import Axios from "axios";
+
+export const login = user => {
+    Axios.post("http://localhost:5000/session/", user)
+};
+
+export const logout = () => {
+    Axios.delete("http://localhost:5000/session/")
+};
+
+export const checkLoggedIn = async preloadedState => {
+    const response = await Axios.get("http://localhost:5000/session/");
+    const { user } = await response.json();
+
+    let preloadedState = {};
+
+    if (user) {
+        preloadedState = {
+            session: user,
+        };
+    }
+    return preloadedState;
+};
