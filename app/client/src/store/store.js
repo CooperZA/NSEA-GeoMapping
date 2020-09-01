5c04df43501283bcc1cfc2a655a604fbc846abfe
CooperZA <zach@raincitysolutions.com>
2020-09-01 13:00:33 -0700

    add reducer function to store

--- app/client/src/store/store.js
+++ app/client/src/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-// import reducer from "../reducers/root";
+import reducer from "../reducers/root";
 
 export default preloadedState => (
   createStore(
-    // reducer,
+    reducer,
     preloadedState,
     applyMiddleware(thunk)
   )
