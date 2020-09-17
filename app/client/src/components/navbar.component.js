47ac9b148655057fb7fa37152fe245d23cfa0b2f
CooperZA <zach@raincitysolutions.com>
2020-09-17 13:17:38 -0700

    remove comments

--- app/client/src/components/navbar.component.js
+++ app/client/src/components/navbar.component.js
@@ -6,23 +6,16 @@ export default class Navbar extends Component {
         return (
             <div className="container">
                 <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                    <Link to="/projects/" className="navbar-brand">NSEA-Project-Map</Link>
-                    {/* <Link to="/" className="navbar-brand">NSEA-Project-Map</Link> */}
+                    <Link to="/projects" className="navbar-brand">NSEA-Project-Map</Link>
                     <div className="collapse navbar-collapse">
-                        {/* home, create project, create project type, create fish type, go to map */}
+                        {/* home, create project, go to map */}
                         <ul className="navbar-nav mr-auto">
                             <li className="navbar-item">
-                                <Link to="/projects/" className="nav-link">Projects</Link>
+                                <Link to="/projects" className="nav-link">Projects</Link>
                             </li>
                             <li className="navbar-item">
                                 <Link to="/projects/create" className="nav-link">Add New Project</Link>
                             </li>
-                            {/* <li className="navbar-item">
-                            <Link to="/projecttype/create" className="nav-link">Add New Project Type</Link>
-                        </li> */}
-                            {/* <li className="navbar-item">
-                            <Link to="/fishtype/create" className="nav-link">Add New Fish Type</Link>
-                        </li> */}
                             <li className="navbar-item">
                                 <Link to="/" className="nav-link">Go To Map</Link>
                             </li>
