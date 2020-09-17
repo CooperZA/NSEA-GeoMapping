8d9b40a34781e893804ebb6c728b318b82d8440e
CooperZA <zach@raincitysolutions.com>
2020-09-17 13:19:18 -0700

    restructure session routes for api

--- app/routes/session.js
+++ app/routes/session.js
@@ -1,46 +1,58 @@
 const router = require('express').Router();
-const Joi = require('joi');
 const Admin = require('../models/admin.model');
+const mongoose = require('mongoose');
 // import functions
 const validations = require('../validations/admin');
 // import { parseError, sessionizeUser } from '../util/helpers';
 const helpers = require('../util/helpers');
 
 // check if user is signed in
-router.route("/").get(({ session: { admin }}, res) => {
-    res.send({ admin });
+router.route("/").get((req, res) => {
+    const admin = req.session.admin;
+    console.log(req.session);
+    res.send(admin);
 });
 
 // create session
 router.route("/").post((req, res) => {
-    const { Username, Password } = req.body;
+    // extract values
+    const Username = String(req.body.Username);
+    const Password = String(req.body.Password);
 
-    Joi.validate({ Username, Password }, validations.signIn)
-        .then(() => {
-            const admin = Admin.findOne({ Username })
-                .then(() => {
-                    if (admin && admin.comparePasswords(Password)) {
-                        const sessionAdmin = helpers.sessionizeUser(admin);
+    // validate inputs
+    const { error, value } = validations.signIn.validate({ Username, Password });
 
-                        req.session.admin = sessionAdmin;
-                        res.send(sessionAdmin);
-                    } else {
-                        throw new Error('Invalid login credentials');
-                    }
-                })
-                .catch(err => res.status(400).send(parseError(err)));
+    // check for errors
+    if (error != undefined) {
+        console.log(error);
+        throw new Error("object did not pass validation");
+    }
+
+    // find admin in DB
+    Admin.findOne({ Username })
+        .then((admin) => {
+            // check that login is valid
+            if (admin && admin.comparePasswords(Password)) {
+                const sessionAdmin = helpers.sessionizeUser(admin);
+
+                req.session.admin = sessionAdmin;
+                req.session.save(() => {
+                    res.send(sessionAdmin);
+                });
+            } else {
+                throw new Error('Invalid login credentials');
+            }
         })
-        .catch(err => res.status(400).send(parseError(err)));
-
+        .catch(err => res.status(400).send(helpers.parseError(err)));
 });
 
 // delete session
-router.route("/").delete(({ session }, res) => {
+router.route("/").delete((req, res) => {
     try {
-        const admin = session.admin;
-    
+        const admin = req.session;
+
         if (admin) {
-            session.destroy(err => {
+            admin.destroy(err => {
                 if (err) throw (err);
                 res.clearCookie(process.env.SESS_NAME);
                 res.send(admin);
@@ -49,7 +61,7 @@ router.route("/").delete(({ session }, res) => {
             throw new Error('something went wrong');
         }
     } catch (err) {
-        res.status(422).send(parseError(err));
+        res.status(422).send(helpers.parseError(err));
     }
 });
 
